33d60b19d206a62f4bbd5ced58544b3fd8bbf90c
Junior Dugue <jrdugue@gmail.com>
2019-05-27 12:40:56 -0400

    styled the tour component mobile/desktop versions, refactored the tour component page and added two new variables for transition and border

--- city-tours/src/components/Navbar/Navbar.js
+++ city-tours/src/components/Navbar/Navbar.js
@@ -5,7 +5,7 @@ import "./navbar.scss";
 export default function Navbar() {
   return (
     <nav className="navbar">
-      <img src="{logo}" alt="city tours company"/>
+      <img src={logo} alt="city tours company"/>
       <ul className="nav-links">
         <li><a href="/" className="nav-link">
           home
--- city-tours/src/components/Tour/Tour.js
+++ city-tours/src/components/Tour/Tour.js
@@ -4,12 +4,31 @@ import React, { Component } from 'react'
 import "./tour.scss";
 
 export default class Tour extends Component {
+  state = {
+    showInfo: false
+  }
+
   render() {
-    console.log(this.props);
+    // console.log(this.props);
+    const {id, city, img, name, info} = this.props.tour;
+    const {removeTour} = this.props;
+
     return (
-      <h1>
-        Hello from tour
-      </h1>
-    )
+      <article className="tour">
+        <div className="img-container">
+          <img src={img} alt="of the tour"/>
+          <span className="close-btn">
+            <i className="fas fa-window-close"></i>
+          </span>
+        </div>
+
+        <div className="tour-info">
+          <h3>{city}</h3>
+          <h4>{name}</h4>
+          <h5>info {" "}<span><i className="fas fa-caret-square-down"></i></span></h5>
+          {this.state.showInfo && <p>{info}</p>}
+        </div>
+      </article>
+    );
   }
 }
